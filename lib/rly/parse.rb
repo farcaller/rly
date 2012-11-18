f83a81c99c3326ae78511d60bcd787ca2b16f191
Vladimir Pouzanov <farcaller@gmail.com>
2012-11-18 11:22:50 +0200

    Added basic parser specs

--- lib/rly/parse.rb
+++ lib/rly/parse.rb
@@ -1,9 +1,40 @@
+require "rly/lex"
+require "rly/parse/grammar"
+
 module Rly
   class Parse
+    attr_reader :lex, :grammar, :lr_table
+
+    def initialize(lex=nil)
+      raise ArgumentError.new("No lexer available") if lex == nil && self.class.lexer_class == nil
+      @lex = lex || self.class.lexer_class.new
+
+      #@grammar = self.class.grammar
+    end
+
 
     class << self
-      private
-      
+      attr_accessor :rules, :grammar, :lexer_class
+
+      def rule(desc, &block)
+        self.rules << [desc, block]
+      end
+
+      def lexer(&block)
+        @lexer_class = Class.new(Lex, &block)
+      end
+
+      def rules
+        @rules ||= []
+      end
+
+      def grammar
+        return @grammar if @grammar
+
+        @grammar = Grammar.new(self.lexer_class.terminals)
+
+        # FIXME ...
+      end
     end
   end
 end
---
+++ spec/parse/parse_spec.rb
@@ -0,0 +1,67 @@
+require "rly"
+require "rly/parse/grammar"
+
+describe Rly::Parse do
+  it "acceps a set of rules" do
+    expect {
+      Class.new(Rly::Parse) do
+        rule 'statement : expression' do |e|
+          @val = e
+        end
+      end
+      } .not_to raise_error
+  end
+
+  it "creates a grammar on first class instantiation" do
+    pending "implement full grammar-parsing stack"
+    testParser = Class.new(Rly::Parse) do
+      rule 'statement : VALUE' do |v|
+        @val = v
+      end
+    end
+    testParser.grammar.should be_nil
+    p = testParser.new(double(Rly::Lex))
+    testParser.grammar.should_not be_nil
+    testParser.grammar.should be_kind_of(Rly::Grammar)
+  end
+
+  it "accepts an instance of lexer as an argument" do
+    testParser = Class.new(Rly::Parse) do
+      rule 'statement : VALUE' do |v|
+        @val = v
+      end
+    end
+    
+    m = double('lexer')
+
+    expect {
+      p = testParser.new(m)
+      p.lex.should == m
+    } .not_to raise_error
+  end
+
+  it "can use built in lexer if one is defined" do
+    testParser = Class.new(Rly::Parse) do
+      lexer do
+        token :FIRST, /[a-z]+/
+      end
+
+      rule 'statement : VALUE' do |v|
+        @val = v
+      end
+    end
+
+    p = testParser.new
+    p.lex.should be_kind_of(Rly::Lex)
+  end
+
+  it "raises error if no lexer is built in and no given" do
+    testParser = Class.new(Rly::Parse) do
+      rule 'statement : VALUE' do |v|
+        @val = v
+      end
+    end
+
+    expect { testParser.new } .to raise_error(ArgumentError)
+  end
+end
