9384721fa5ac17ae71a651e2e6d9451cc1bfcb15
Vladimir Pouzanov <farcaller@gmail.com>
2012-11-18 14:23:26 +0200

    YaccSymbol now looks prettier in the console logs

--- lib/rly/parse/yacc_symbol.rb
+++ lib/rly/parse/yacc_symbol.rb
@@ -2,5 +2,12 @@ module Rly
   class YaccSymbol
     attr_accessor :type, :value, :lineno, :endlineno, :lexpos, :endlexpos
 
+    def to_s
+      @value.to_s
+    end
+
+    def inspect
+      "#<YaccSymbol #{@type} '#{@value}'>"
+    end
   end
 end
