dcdf72f2513e834b9df5afd1e4e57d9f92236cfb
Vladimir Pouzanov <farcaller@gmail.com>
2012-11-19 14:10:51 +0200

    Added a test tokenization to RuleParser

--- spec/parse/rule_parser_spec.rb
+++ spec/parse/rule_parser_spec.rb
@@ -17,4 +17,15 @@ describe Rly::RuleParser do
     productions[2].should == [:expression, [:expression, '*', :expression], nil]
     productions[3].should == [:expression, [:expression, '/', :expression], nil]
   end
+
+  it "tokenizes the rule correctly" do
+    s = 'maybe_superclasses : ":" superclasses |'
+    l = Rly::RuleParser.lexer_class.new(s)
+
+    l.next.type.should == :ID
+    l.next.type.should == ':'
+    l.next.type.should == :LITERAL
+    l.next.type.should == :ID
+    l.next.type.should == '|'
+  end
 end
