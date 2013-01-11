a2b9afe9e9d81f83ea0e3b5de5854e0c506cc7c6
Vladimir Pouzanov <farcaller@gmail.com>
2013-01-11 23:55:13 +0000

    Travis will nag on coverage now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,10 @@ require "rubygems"
 require "bundler/setup"
 
 require "simplecov"
+SimpleCov.minimum_coverage 100
 SimpleCov.start do
   add_filter '/spec/'
-end if ENV["COVERAGE"]
+end if ENV["COVERAGE"] == 'true'
 
 begin
   require "pry-nav"
